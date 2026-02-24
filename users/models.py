94fff032ad0a19bf688e34c917a0cd129c642322
Carlota Moreno <carlota.moreno3@alu.uclm.es>
2026-02-24 16:02:46 +0100

    Remove db.sqlite3 and add gitignore

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        USER = "USER", "User"
+
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.USER)
