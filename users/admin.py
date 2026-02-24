e359b95d2c7afb63bf663c66ca9c0ad675732b1f
Carlota Moreno <carlota.moreno3@alu.uclm.es>
2026-02-25 00:52:49 +0100

    Editados los cambios en la configuración de los usuarios

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ("email",)
+    list_display = ("email", "first_name", "last_name", "role", "time_credits", "is_staff", "is_active")
+    search_fields = ("email", "first_name", "last_name")
+    list_filter = ("role", "is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        ("TimeBank", {"fields": ("time_credits", "role")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_staff", "is_active", "role"),
+        }),
+    )
+
+    readonly_fields = ("date_joined",)
